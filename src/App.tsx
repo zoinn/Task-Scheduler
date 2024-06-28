8e7136988e6530696e8ce614dd036fc84dfac020
N0935198 <N0935198@ntu.ac.uk>
2024-06-28 08:41:30 +0100

    useState implmentation for task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { MantineProvider, createTheme} from '@mantine/core';
 
@@ -6,13 +7,31 @@ const theme = createTheme({
 });
 
 function App() {
+  const [tasks, setTasks] = useState(['Task 1']);
+
+  function addTask() {
+  setTasks((prevTasks) => [...prevTasks, `Task ${prevTasks.length + 1}`]);
+  // Add a task
+  // add to a list like form?
+  // add a new element to page
+  };
+
+  function addSubTask() {
+  // add subtask to a task
+  };
+
   return (
     <MantineProvider theme={theme}>
       <div className="task-scheduler">
         <h1>Task Scheduler</h1>
         <div className="tasks">
           <h2>Tasks</h2>
-          <button className="button">Add Task</button>
+          <button className="button" onClick={() => addTask()}>Add Task</button>
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}><button aria-label="buttontask">{task}</button></li>
+            ))}
+          </ul>
           <div className="subtasks">
             <h2>Sub Tasks</h2>
             <button className="button">Add Sub Task</button>
